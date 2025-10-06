c036bf79dafc88702146bd0327f4eb991e7c74cc
staffgha <ghall@roundhayschool.com>
2025-10-06 14:24:41 +0100

    made a small red screen

--- gameloopwithfunctions.py
+++ gameloopwithfunctions.py
@@ -8,15 +8,16 @@ def update():
     pass
 
 
-def draw():
-    pass
+def draw(screen):
+    screen.fill((255,0,0))
+    pygame.display.flip()
 
 
 def main():
-    screen = pygame.()s
+    screen = pygame.display.set_mode((200,200))
     while True:
         events()
         update()
-        draw()
+        draw(screen)
 
 main()
