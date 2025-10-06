26330a0bff1c0b508ed4cbe27e12def76a33ac51
Mr Hall <mr-hall@users.noreply.github.com>
2025-10-06 14:31:07 +0100

    changed some bits, added comments

--- gameloopwithfunctions.py
+++ gameloopwithfunctions.py
@@ -11,11 +11,14 @@ def update():
 
 
 def draw(screen):
+    #drawing on screen
     screen.fill((255,0,0))
     pygame.display.flip()
 
 
 def main():
+    #my main program
+    pygame.init()
     screen = pygame.display.set_mode((200,200))
     done = False
     while not done:
